f1d0f0b8f67f8dfcff7b3876b4785d410e0352ac
Betxi <pedro.alson.h@gmail.com>
2015-10-23 12:49:46 +0200

    cacheSolve and makeCacheMatrix
    Some missing # in the previous commit. An Example is showed.

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,8 @@
 ## The two following R functions allow us to evaluate a mathematical function at a particular value of its argument, store the result of this evaluation  and recuperate it when needed. 
  
-
-## The first R function "makeCacheMatrix" uses the R function "solve" (instead of "mean").  The place where the result of solve (applied to some defined argument) will be stored is called "hueco", which in spanish means "hole". 
-The R rutine makeCacheMatrix returns a list of four R functions. 
+## The first R function "makeCacheMatrix" uses the R function "solve" (instead of "mean").  
+#The place where the result of solve (applied to some defined argument) will be stored is called "hueco", which in spanish means "hole". 
+#The R rutine makeCacheMatrix returns a list of four R functions. 
 
 makeCacheMatrix<- function(ma=numeric())
   { 
@@ -10,9 +10,9 @@ hueco<-NULL
 set<-function(y) 
 {
 	
-## In the next instrucction ma is set as a variable whose definition will be assigned by a search through the parent environemnt from wich it is called.
+## In the next instruction ma is set as a variable whose definition will be assigned by a search through the parent environenment from wich it is called.
 ## In the global environment there is no need to set ma. This is why I puted the following line as a comment. 
-  # ma <<- y 
+   #ma <<- y 
 ## Idem for hueco: but is necessary to set hueco, because it is a free variable (it is not an argument of makeCacheMatrix ) 
 ## Notice the difference between "hueco<-NULL" and "hueco<<-NULL".
   hueco <<- NULL
@@ -32,9 +32,9 @@ getinvma<-  function () hueco
 
 
 ## If ma is a matrix. You can run
-          xx<-makeCacheMatrix(ma)   (once)
-          cacheSolve(xx)   (any time you need the value of solve(ma))
-The first time you run cacheSolve(xx) the message "getting cached data" does not appear, because this time solve(ma) must be calculated. The second time the message appears because solve(ma) value is already in the caché.
+##          xx<-makeCacheMatrix(ma)   (once)
+##          cacheSolve(xx)   (any time you need the value of solve(ma))
+##The first time you run cacheSolve(xx) the message "getting cached data" does not appear, because this time solve(ma) must be calculated. The second time the message appears because solve(ma) value is already in the caché.
 
 ## ¿¿?? I do not see any advantage to do things that way. For me it looks simpler: invma<-solve(ma) and use invma any time we need solve(ma). 
  
@@ -50,3 +50,16 @@ cacheSolve<-function(invma, ...) {
   invma$setinvma(hueco)
   hueco
 }
+
+#EXAMPLE
+
+# > xx<-makeCacheMatrix(matrix(1:4,2,2))
+# > cacheSolve(xx)
+#     [,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+#> cacheSolve(xx)
+#getting cached data
+#     [,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
